04aa5f48a0bae235b37eb2b086915b0e8084e635
plomatteo <matte.polo@gmail.com>
2023-03-12 11:35:13 +0100

    add post dario

--- basedir/src/components/Blog.js
+++ basedir/src/components/Blog.js
@@ -16,6 +16,27 @@ const Blog = () => {
           <div className="col-md-6 m-15px-tb">
             <div className="blog-grid">
               <div className="blog-img">
+                <Link href="/single-blog-3">
+                  <a>
+                    <img src="static/img/mare-montagna.png" title="" alt="" />
+                  </a>
+                </Link>
+              </div>
+              <div className="blog-info">
+                <div className="meta">11/MAR/2023</div>
+                <h6>
+                  <Link href="/single-blog-3">
+                    <a>
+                      Finanziamenti per impianti di risalita a bassa quota sì, escavi delle cavane no
+                    </a>
+                  </Link>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 m-15px-tb">
+            <div className="blog-grid">
+              <div className="blog-img">
                 <Link href="/single-blog-2">
                   <a>
                     <img src="static/img/precarieta.jpeg" title="" alt="" />
